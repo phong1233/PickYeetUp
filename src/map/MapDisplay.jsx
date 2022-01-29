921c253288d4f63d6c461d15c5e96915d41a9a1e
Sebastien <sebastienbn8@gmail.com>
2022-01-29 14:20:31 -0500

    Added context

--- src/map/MapDisplay.jsx
+++ src/map/MapDisplay.jsx
@@ -1,15 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { get, set } from 'idb-keyval';
 import Map from './Map';
+import { useCustomer } from '../contexts/CustomerContext';
 
 export const MapDisplay = () => {
 
+  const customer = useCustomer();
+
     const initGeocoder = async ({ maps }) => {
         const geocoder = new maps.Geocoder();
+        setGeoc(geocoder);
         await getStores(geocoder);
       };
     
+      const [geoc, setGeoc] = useState();
       const [stores, setStores] = useState([]);
+      const [customerState, setCustomerState] = useState();
+
+      const loadCustomer = async() => {
+        console.log(customer)
+        if (customer.customer){
+          let saved = await get(customer.customer.customerEmailAddress);
+            if(saved === undefined){
+              const geo = await geoc.geocode({address: customer.customer.customerAddress})
+              const lat = geo.results[0].geometry.location.lat()
+              const lng = geo.results[0].geometry.location.lng()
+              saved = {lat: lat, lng: lng}
+              await set(customer.customer.customerEmailAddress, saved)
+            } 
+            setCustomerState({...customer.customer, location: saved})
+        }
+      }
+
+      useEffect(() => {
+        loadCustomer();
+      }, [customer]);
 
       const getStores = async (geocoder) => {
         const request = await fetch(`https://sapstore.conuhacks.io/stores`, {
@@ -36,7 +61,7 @@ export const MapDisplay = () => {
       }
       
     return (
-            <Map initGeocoder={initGeocoder} storesLocation={stores}/>
+            <Map initGeocoder={initGeocoder} storesLocation={stores} customer={customerState}/>
           );
 }
 
