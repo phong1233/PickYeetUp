875f53cc3129d5fcf89d19bf75e8cd2c3e94eced
Sebastien <sebastienbn8@gmail.com>
2022-01-29 12:49:13 -0500

    Fixed getting the stores

--- src/map/MapDisplay.jsx
+++ src/map/MapDisplay.jsx
@@ -4,43 +4,37 @@ import Map from './Map';
 
 export const MapDisplay = () => {
 
-    const initGeocoder = ({ maps }) => {
+    const initGeocoder = async ({ maps }) => {
         const geocoder = new maps.Geocoder();
-        setGeocoder(geocoder);
+        await getStores(geocoder);
       };
     
       const [stores, setStores] = useState([]);
-      const [geocoder, setGeocoder] = useState([]);
-    
-      useEffect(() => {
-        const getStores = async () => {
-          const request = await fetch(`https://sapstore.conuhacks.io/stores`, {
-            method: 'GET'
-          });
-          const response = (await request.json())
-    
-          if (geocoder !== undefined){
-            for (let index = 0; index < response.length; index++) {
-              const s = response[index];
-              let saved = await get(s.storeId);
 
-              if(saved == undefined){
-                const geo = await geocoder.geocode({address: s.address})
-                const lat = geo.results[0].geometry.location.lat()
-                const lng = geo.results[0].geometry.location.lng()
-                saved = {lat: lat, lng: lng}
-                await set(s.storeId, saved)
-              } 
-              s['location'] = saved
-            }
+      const getStores = async (geocoder) => {
+        const request = await fetch(`https://sapstore.conuhacks.io/stores`, {
+          method: 'GET'
+        });
+        const response = (await request.json())
+  
+        if (geocoder !== undefined){
+          for (let index = 0; index < response.length; index++) {
+            const s = response[index];
+            let saved = await get(s.storeId);
+            if(saved == undefined){
+              const geo = await geocoder.geocode({address: s.address})
+              const lat = geo.results[0].geometry.location.lat()
+              const lng = geo.results[0].geometry.location.lng()
+              saved = {lat: lat, lng: lng}
+              await set(s.storeId, saved)
+            } 
+            s['location'] = saved
           }
-    
-          setStores(response)
         }
-        getStores();
-    
-      }, []);
-
+  
+        setStores(response)
+      }
+      
     return (
             <Map initGeocoder={initGeocoder} storesLocation={stores}/>
           );
